d83ce3faf50a9930d0cf4d8395d9a3024264b006
XiobaiiShikii <regifrdm@gmail.com>
2026-05-03 05:21:42 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ let allData = [], currentLang = 'id', activeFilter = 'all';
 
 async function init() {
   try {
-    const res = await fetch('/api/admin-airdrop');
+    const res = await fetch('/api/airdrops');
     if (!res.ok) throw new Error(`HTTP ${res.status}`);
     const data = await res.json();
     if (!Array.isArray(data)) throw new Error('Data tidak valid');
