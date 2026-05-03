c59628f5096e10da72b66b5fdfb9b0f56f62383a
XiobaiiShikii <regifrdm@gmail.com>
2026-05-03 11:01:26 +0700

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -14,44 +14,22 @@ export default async function handler(req, res) {
 
     let xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${baseUrl}/</loc>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/unlocks.html</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/exchanges.html</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/reward.html</loc>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>${baseUrl}/docs.html</loc>
-    <priority>0.8</priority>
-  </url>`;
+  <url><loc>${baseUrl}/</loc><priority>1.0</priority></url>
+  <url><loc>${baseUrl}/unlocks.html</loc><priority>0.8</priority></url>
+  <url><loc>${baseUrl}/exchanges.html</loc><priority>0.8</priority></url>
+  <url><loc>${baseUrl}/reward.html</loc><priority>0.8</priority></url>
+  <url><loc>${baseUrl}/docs.html</loc><priority>0.8</priority></url>`;
 
     if (airdrops && airdrops.length > 0) {
       airdrops.forEach((item) => {
-        xml += `
-  <url>
-    <loc>${baseUrl}/detail.html?id=${item.id}</loc>
-    <priority>0.8</priority>
-  </url>`;
+        xml += `\n  <url><loc>${baseUrl}/detail.html?id=${item.id}</loc><priority>0.8</priority></url>`;
       });
     }
 
-    xml += `
-</urlset>`;
-
+    xml += `\n</urlset>`;
     res.setHeader('Content-Type', 'text/xml');
     res.status(200).send(xml);
   } catch (error) {
-    res.setHeader('Content-Type', 'application/json');
     res.status(500).json({ error: error.message });
   }
 }
